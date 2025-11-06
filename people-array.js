0534ec053ef135aa593295a30a258e2e675384fa
Your Name <you@example.com>
2025-11-06 10:31:10 +0200

    done task 3

---
+++ people-array.js
@@ -0,0 +1,16 @@
+const p_array = [
+    {
+        'name': 'Maria',
+        'age': 16,
+        'is_student': true,
+    },
+    {
+        'name': 'Angela',
+        'age': 23,
+        'is_student': false,
+    }
+]
+
+for (person in p_array) {
+    console.log('Name: ' + person.name);
+}
